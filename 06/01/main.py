1d34776efdb2c1190635aebcb5031dd5cdd8f831
PepijnBullens <pepijnpersoonlijk@gmail.com>
2026-02-27 00:10:08 +0100

    Added day 06

--- 06/01/main.py
+++ 06/01/main.py
@@ -1,41 +1,59 @@
+import math
+
 def readLines(fileName):
     array = []
     
     with open(fileName, 'r') as file:
         for line in file:
-            array.append(line.strip())
+            array.append(line)
 
     return array
 
 def splitLines(lines):
     sizes = []
     lastRow = lines[-1]
+    
     size = 1
 
-    for i in range(len(lastRow)):
-        if i == 0:
-            continue
-
-        if lastRow[i] == "+" or lastRow[i] == "*":
+    for i in range(len(lastRow) - 1):
+        if lastRow[i] == "+" or lastRow[i] == "*" and size > 1:
             sizes.append(size - 1)
-            size = 0
-
+            size = 1
         size += 1
+    sizes.append(size)
 
     return sizes
 
 
 
 def main():
-    lines = readLines('../test-input.txt')
+    lines = readLines('../input.txt')
     sizes = splitLines(lines)
 
+    equations = []
+
     for y in range(len(lines)):
-        for x in range(len(lines[y])):
-            numbers = []
+        sortedEquations = []
 
-                numbers.append(lines[y][x + sizes[]])
+        for i in range(len(sizes)):
+            start = sum(sizes[:i])
+            end = start + sizes[i]
+            sortedEquations.append(lines[y][start:end].strip())
+        
+        equations.append(sortedEquations)
 
-            print(numbers)
+    equations = [list(col) for col in zip(*equations)]
+
+    count = 0
+
+    for digits in equations:
+        int_digits = [int(d) for d in digits[:-1] if d.strip()]
+        if digits[-1] == "*":
+            result = math.prod(int_digits)
+        else:
+            result = sum(int_digits)
+        count += result
+
+    print(f"COUNT: {count}")
 
 main()
--- 06/02/main.py
+++ 06/02/main.py
@@ -3,46 +3,57 @@ def readLines(fileName):
     
     with open(fileName, 'r') as file:
         for line in file:
-            array.append(line.strip())
+            array.append(line)
 
     return array
 
 def splitLines(lines):
-    ranges = []
-    available = []
+    sizes = []
+    lastRow = lines[-1]
+    
+    size = 1
 
-    switch = False
-    for line in lines:
-        if line == "":
-            switch = True
-            continue
+    for i in range(len(lastRow) - 1):
+        if lastRow[i] == "+" or lastRow[i] == "*" and size > 1:
+            sizes.append(size - 1)
+            size = 1
+        size += 1
+    sizes.append(size)
 
-        if switch:
-            available.append(line)
-        else:
-            ranges.append(line)
+    return sizes
 
-    return [ranges, available]
+
 
 def main():
     lines = readLines('../input.txt')
-    [ranges, available] = splitLines(lines)
+    sizes = splitLines(lines)
 
-    parsedRanges = []
-    for r in ranges:
-        start, end = map(int, r.split("-"))
-        parsedRanges.append((start, end))
+    count = 0
 
-    parsedRanges.sort()
+    for i in range(len(sizes)):
+        isAddition = lines[-1][sum(sizes[:i])] == "+"
 
-    merged = []
-    for start, end in parsedRanges:
-        if merged and start <= merged[-1][1] + 1:
-            merged[-1] = (merged[-1][0], max(merged[-1][1], end))
-        else:
-            merged.append((start, end))
+        total = 0
 
-    count = sum(end - start + 1 for start, end in merged)
+        for j in range(sizes[i]):
+            store = ""
+
+            for y in range(len(lines)):
+                value = lines[y][sum(sizes[:i]) + j]
+                if value.isnumeric():
+                    store += value
+
+            if isAddition and store.isnumeric():
+                total += int(store)
+            elif not isAddition and store.isnumeric():
+                if total == 0:
+                    total = int(store)
+                else:
+                    total *= int(store)
+
+        count += total
+
     print(f"COUNT: {count}")
+
 
 main()
